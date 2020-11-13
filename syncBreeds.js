c0e17591bd3574d7f31692bedc5b371214a4b10e
Angus Kellett <anguskellett@hotmail.com>
2020-11-13 05:46:23 +0000

    adds module.exports line and removes test case

--- syncBreeds.js
+++ syncBreeds.js
@@ -9,7 +9,9 @@ const breedDetails = function(breed) {
   // can simply return it (easy peezee, butter squeezy) ...
   return catBreeds[breed];
 };
-
+/*
 // get the return value right away from the function
 const bombay = breedDetails('Bombay');
 console.log(bombay); //=> prints out the description for that breed
+*/
+module.exports = breedDetails;
